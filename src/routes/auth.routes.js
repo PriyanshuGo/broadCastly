b9a89f2344f076d4f2103ca40f6e2a29b5cce7b4
PriyanshuGo <priyanshusinghrawat84@gmail.com>
2026-07-21 12:56:46 +0530

    fix: auth routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,18 +1,15 @@
 import express from "express";
 const router = express.Router();
 
-import {
-    login,
-    refreshAccessToken,
-    logout,
-} from "../controllers/auth.controller.js";
+
 import {googleAuth} from "../controllers/auth/googleAuth.controller.js"
+import {login} from "../controllers/auth/login.controller.js"
+import {refreshAccessToken} from "../controllers/auth/refreshAccessToken.controller.js"
+import {logout} from "../controllers/auth/logout.controller.js"
 
 router.post("/google", googleAuth);
 router.post("/login", login);
-
 router.post("/refresh-token", refreshAccessToken);
-
 router.post("/logout", logout);
 
 export default router;
